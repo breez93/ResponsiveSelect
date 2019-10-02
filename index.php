947ee4b9a6f57934e7f7f2edb2f497e82a0d52be
breez93 <48477753+breez93@users.noreply.github.com>
2019-10-02 18:50:31 +0100

    Create index.php
    Co-Authored-By: TiagoAmaral53 <tiagoamaral53@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+<style type="text/css">;
+?>
+<form action="index.php" method="GET">
+
+<select name="Concelhos" id="Conc">
+<option value="Santarem">Santarem</option>
+<option value="Santarem">Santarem</option>
+<option value="Santarem">Santarem</option>
+
+
+</select>
+<select name="Concelhos" id="Conc">
+<option value="Santarem">Santarem</option>
+<option value="Santarem">Santarem</option>
+<option value="Santarem">Santarem</option>
+
+
+</select>
+
+</form>
+</body>
+</html>
