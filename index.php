f0202cba1a78e63fe404352366f20cbd8e91e60f
breez93 <48477753+breez93@users.noreply.github.com>
2019-10-02 21:57:24 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,26 +7,64 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-<style type="text/css">;
-?>
-<form action="index.php" method="GET">
+    <h3>.................Responsive Select..............</h3>
+    <br>
+    <br>
+    <hr>
+    
+    <form action="index.php" method="GET">
+        <?php
+        if(isset($_GET["Enviar"])){
+            $distritos;
+            $concelhos;
 
-<select name="Concelhos" id="Conc">
-<option value="Santarem">Santarem</option>
-<option value="Santarem">Santarem</option>
-<option value="Santarem">Santarem</option>
+        }
+        
+        
+        ?>
+        
+        <select name="Distritos" id="Dist">
+        <option value="Santarem" id="dist1">Santarem</option>
+        <option value="Evora" id="dist2">Evora</option>
+        <option value="Lisboa" id="dist3">Lisboa</option>
+        <?php
+        
+        $distritos = $_GET["Distritos"];
+        $concelhos = $_GET["Conc"];
+        ?>
+    </select>
+    <select name="Concelhos" id="Conc">
+        <option value="Torres Novas" id="conc1">Torres Novas</option>
+        <option value="Mourao" id="conc2">Mourao</option>
+        <option value="Cascais" id="conc3">Cascais</option>
+        <?php
+        
+        if($distritos=="Santarem"){
+            $concelhos="Torres Novas";
+        }
+            elseif($distritos=="Evora"){
+                $concelhos="Mourao";
+            }
+            else{
+                $concelhos="Cascais";
+            }
+        ?>
+        <input type="submit" value="Enviar">
+     
+    </select>
+    </form>
+    <br>
+    <br>
+    <p>Resultado:    </p>
+    <hr>
+    <?php
+        
+        echo"O distrito e:   ".$distritos.", e o seu respetivo concelho e:  ".$concelhos;
+        
+        
+        ?>
 
 
-</select>
-<select name="Concelhos" id="Conc">
-<option value="Santarem">Santarem</option>
-<option value="Santarem">Santarem</option>
-<option value="Santarem">Santarem</option>
-
-
-</select>
-
-</form>
 </body>
+
 </html>
